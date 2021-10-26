5b20474e5cc514248c1aa2bf71cc351150621158
Gustavo Duarte <slepziin@gmail.com>
2021-10-25 21:09:30 -0300

    Fist commit

---
+++ js/javascript.js
@@ -0,0 +1,14 @@
+//exibir o saiba mais
+function saiba() {
+  document.getElementById("conteudo").innerHTML = "Começei a estudar progamação no final de 2020, começei focado no front-end e até hoje ainda busco aprender mais, entretanto hoje em dia ainda quero aprender back-end, para deixar meus sites mais completos"
+
+
+}
+
+//aumentar a minha foto de perfil
+
+function aumentar() {
+   document.getElementById('foto').style.border = "4px solid #6A5ACD"
+
+}
+
